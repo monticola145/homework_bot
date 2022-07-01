944b97e977b186a850acaa5af1d6d187c32c2df8
unknown <jandiev@bk.ru>
2022-07-02 00:03:47 +0300

    Ревью №1

---
+++ exceptions.py
@@ -0,0 +1,20 @@
+class NotForSending(Exception):
+    """Ошибки, о которых не пишется в Telegram."""
+
+    pass
+
+
+class IsForSending(Exception):
+    """Ошибки, о которых уведомляется в Telegram"""
+
+    pass
+
+
+class EmptyAPIAnswer(NotForSending):
+
+    pass
+
+
+class WrongResponseCode(IsForSending):
+
+    pass
--- homework.py
+++ homework.py
@@ -1,11 +1,20 @@
 import logging
 import os
 import time
+import sys
 from http import HTTPStatus
 
 import requests
 from dotenv import load_dotenv
 from telegram import Bot
+import telegram
+
+import exceptions
+
+NotForSending = exceptions.NotForSending
+IsForSending = exceptions.IsForSending
+EmptyAPIAnswer = exceptions.EmptyAPIAnswer
+WrongResponseCode = exceptions.WrongResponseCode
 
 load_dotenv()
 
@@ -17,40 +26,38 @@ ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 
-HOMEWORK_STATUSES = {
+HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
 logging.basicConfig(
-    handlers=[logging.StreamHandler()],
+    # handlers=[logging.StreamHandler()],
+    # никак не могу справиться со StreamHandler
+    # без него всё работает
     level=logging.INFO,
-    format='%(asctime)s, %(levelname)s, %(message)s'
+    filename='main.log',
+    format='%(asctime)s, %(levelname)s, %(funcName)s, %(message)s',
+    encoding='UTF-8',
+    filemode='w'
 )
+# и если прятать там же, где и main()
 
 logger = logging.getLogger(__name__)
-detected_errors = []
+# если это удалить, то всё ломается
 
 
 def send_message(bot, message):
     """Функция отправки сообщений."""
     try:
+        logger.info('Отправка сообщения...')
         bot.send_message(TELEGRAM_CHAT_ID, message)
+    except telegram.error.TelegramError(NotForSending) as error:
+        raise error(f'Сбой при отправке сообщения в Telegram: {error}')
+        #  так?
+    else:
         logger.info('Сообщение успешно отправлено')
-    except Exception:
-        logging.error('Сбой при отправке сообщения в Telegram')
-
-
-def send_error(bot, message):
-    """Отправляет информацию об ошибках."""
-    logger.error(message)
-    if message not in detected_errors:
-        try:
-            send_message(bot, message)
-            detected_errors.append(message)
-        except Exception:
-            logger.info('Отправка сообщения не состоялась')
 
 
 def get_api_answer(current_timestamp):
@@ -58,26 +65,39 @@ def get_api_answer(current_timestamp):
     timestamp = current_timestamp
     params = {'from_date': timestamp}
     headers = HEADERS
-    response = requests.get(ENDPOINT, headers=headers, params=params)
+    # со словарём не понял:
+    # dict = dict(params={'from_date': timestamp}, headers=HEADERS)
+    # а потом:
+    # response = requests.get(ENDPOINT, headers=dict.keys('headers'),
+    # params=dict.keys('params'))
+    # ?
     try:
+        response = requests.get(ENDPOINT, headers=headers, params=params)
+        logger.info('Запос к API...')
         if response.status_code != HTTPStatus.OK:
-            raise Exception('Нет подключения к API')
-            logging.error('Обнаружена недоступность эндпоинта')
-    except Exception:
-        raise Exception('Ошибка API')
-        logging.error('Сбой при запросе к эндпоинту')
-    return response.json()
+            raise WrongResponseCode(
+                f'Код:{response.status_code}/{response.text}'
+            )
+        else:
+            return response.json()
+    except ConnectionError as error:
+        raise error(
+            'Тут я распаковываю гипотетический словарь,\
+            приведённый сверху в #?')
 
 
 def check_response(response):
     """Функция проверки ответа API."""
+    logger.info('Гачало проверки ответа API...')
     if not isinstance(response, dict):
         raise TypeError('API != dict')
     if ['homeworks'][0] not in response:
-        raise IndexError('Такой домашки нет')
-        logging.error('Отсутствие ожидаемых ключей в ответе API')
-    homework = response.get('homeworks')[0]
-    return homework
+        raise EmptyAPIAnswer('Такой домашки нет')
+    homework = response.get('homeworks')
+    if not isinstance(homework, list):
+        raise KeyError('API != list')
+    else:
+        return homework
 
 
 def parse_status(homework):
@@ -86,43 +106,74 @@ def parse_status(homework):
     for status in statuses:
         if status not in homework:
             raise KeyError('Неизвестный статус домашки')
-            logging.error('Недокументированный статус домашней работы')
-
-    homework_name = homework['homework_name']
-    homework_status = homework['status']
-
-    verdict = HOMEWORK_STATUSES[homework_status]
-
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    if homework_status not in HOMEWORK_VERDICTS:
+        raise ValueError('Неизвестный статус')
+    verdict = HOMEWORK_VERDICTS[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    #  return (f'Изменился статус проверки работы "{per1}{per2}"'.format(
+    #  per1=homework_name, per2=HOMEWORK_VERDICTS[homework_status]))
+    #  так?
 
 
 def check_tokens():
     """Функция проверки наличия токенов."""
-    if PRACTICUM_TOKEN and TELEGRAM_TOKEN is not None:
+    if all([PRACTICUM_TOKEN and TELEGRAM_TOKEN]):
         return True
     else:
         logging.critical('Отсутствие ожидаемых ключей в ответе API')
         return False
+    # ещё видел такой вариант:
+    # tokens = [PRACTICUM_TOKEN, TELEGRAM_TOKEN]
+    # return None not in vars
 
 
 def main():
     """Основная логика работы бота."""
-    bot = Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = 0
-    while True:
-        try:
-            response = get_api_answer(current_timestamp)
-            homework = check_response(response)
-            if homework is not None:
-                message = parse_status(homework)
-                if message is not None:
+    if check_tokens() is False:
+        logger.critical('Токены не найдены!')
+        sys.exit(1)
+        # прочитал инфу по ссылке, но не очень понял
+        # поищу ещё в интернете
+        # пока так
+    else:
+        bot = Bot(token=TELEGRAM_TOKEN)
+        current_timestamp = int(time.time())
+        current_report = {'name': 'message'}
+        prev_report = {'name': 'message'}
+        while True:
+            try:
+                response = get_api_answer(current_timestamp)
+                # вот тут не понял про дату и умолчательное
+                homework = check_response(response)
+                if homework:
+                    homework = response.get('homeworks')[0]
+                    message = parse_status(homework)
+                    current_report[homework['name']] = message
+                    if message is not None:
+                        if current_report != prev_report:
+                            send_message(bot, message)
+                            prev_report = current_report.copy()
+                else:
+                    current_report[homework['name']] = 'Нет новых статусов'
+                    logging.info('Нет новых статусов')
+                # так?
+                time.sleep(RETRY_TIME)
+            except (EmptyAPIAnswer,
+                    NotForSending) as error:
+                logging.debug(f'Ошибка не для пересылки: {error}')
+
+            except Exception:
+                logging.critical('Сбой запуска программы')
+                current_report[homework['name']] = 'Сбой запуска программы'
+                if current_report != prev_report:
                     send_message(bot, message)
-            time.sleep(RETRY_TIME)
-        except Exception:
-            logging.critical('Сбой запуска программы')
-            send_error(bot, message)
-            time.sleep(RETRY_TIME)
+                    prev_report = current_report.copy()
+            finally:
+                time.sleep(RETRY_TIME)
 
 
 if __name__ == '__main__':
-    main()
+    main(), logging
+    # так прятать?
